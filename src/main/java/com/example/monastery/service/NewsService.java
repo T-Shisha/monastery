8cbe37afb58ce721db486c01211b1432c9783d94
Tanya <tanya.shisha2001@gmail.com>
2022-05-08 22:57:03 +0300

    add functionality for image

--- src/main/java/com/example/monastery/service/NewsService.java
+++ src/main/java/com/example/monastery/service/NewsService.java
@@ -4,16 +4,29 @@ import com.example.monastery.dao.model.House;
 import com.example.monastery.dao.model.News;
 import com.example.monastery.dao.repository.HouseRepository;
 import com.example.monastery.dao.repository.NewsRepository;
+import com.example.monastery.properties.StorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class NewsService {
+    private final Path rootLocation;
+
+    public NewsService(StorageProperties properties) {
+        this.rootLocation = Paths.get(properties.getLocation());
+    }
+
     @Autowired
     private NewsRepository newsRepository;
     @Autowired
@@ -35,11 +48,10 @@ public class NewsService {
         newsRepository.delete(news);
     }
     public void save(News news, MultipartFile file){
-
+       news.setImageLocation(store(file));
        save(news);
     }
     public void save(News news){
-
         news.setDate(LocalDateTime.now());
         newsRepository.save(news);
     }
@@ -47,6 +59,29 @@ public class NewsService {
     public boolean isExist(Long id) {
         return newsRepository.existsById(id);
     }
-//    private String uploadPicture(Martip)
+    public String store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new RuntimeException("Failed to store empty file.");
+            }
+            Path destinationFile = this.rootLocation.resolve(
+                            Paths.get(file.getOriginalFilename()))
+                    .normalize().toAbsolutePath();
+            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check
+                throw new RuntimeException(
+                        "Cannot store file outside current directory.");
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+                return rootLocation.getFileName()+file.getOriginalFilename();
+
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to store file.", e);
+        }
+    }
 
 }
